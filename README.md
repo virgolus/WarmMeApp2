88f087f93a3df148047e41cf874a20d5dcf3e2e7
virgolus <virgolus@gmail.com>
2019-11-04 19:23:50 +0100

    refactoring 2

--- README.md
+++
@@ -1,1 +0,0 @@
-# WarmMeApp2
--- nodejs/data/Sensors.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var SensorsSchema = new mongoose.Schema({
-    id: Number,
-    area: String
-},{collection: 'sensors'});
-
-mongoose.model('Sensors', SensorsSchema);
-
-module.exports = mongoose.model('Sensors');
